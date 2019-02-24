0b0dae0b977d994a8da26a87fc2400dad3b1c737
JerusalemProgramming.com <jerusalemprogramming@gmail.com>
2019-02-24 10:20:20 +0200

    Create PythonVsGoVsJavascript.py

---
+++ PythonVsGoVsJavascript.py
@@ -0,0 +1,226 @@
+## PYTHON VS. GO (GOLANG) VS. JAVASCRIPT EQUIVALENTS
+## WORK-IN-PROGRESS
+## CHECK BACK SOON FOR UPDATES; PLEASE COME BACK AGAIN SOON
+## PLEASE VISIT OUR WEB SITES (OUR PROBLEM-SOLVING PROGRAMMING, CODING, & DEVELOPMENT SERVICES ARE AVAILABLE FOR HIRE):
+## www.JerusalemProgrammer.com
+## www.JerusalemProgrammers.com
+## www.JerusalemProgramming.com
+
+
+## DECLARE VARIABLES
+## DECLARE VARIABLES
+## DECLARE VARIABLES
+
+## PYTHON
+AnyVariableName = "Whatever String, Number, or other Object"
+
+## JAVASCRIPT
+var AnyVariableName = "Whatever String, Number, or other Object";
+let AnyVariableName = "Whatever String, Number, or other Object";
+const AnyVariableName = "ES6 - Whatever String, Number, or other Object in ES6";
+
+## GO (GOLANG)
+
+
+## PRINT TO CONSOLE
+## PRINT TO CONSOLE
+## PRINT TO CONSOLE
+
+## PYTHON
+print(AnyVariableName)
+
+## JAVASCRIPT
+console.log(AnyVariableName);
+
+## GO (GOLANG)
+
+
+## CHECK TYPE OF OBJECT
+## CHECK TYPE OF OBJECT
+## CHECK TYPE OF OBJECT
+
+## PYTHON
+type(AnyVariableName)
+
+## JAVASCRIPT
+typeof(AnyVariableName);
+
+## GO (GOLANG)
+
+
+
+## RETURN LENGTH OF OBJECT
+## RETURN LENGTH OF OBJECT
+## RETURN LENGTH OF OBJECT
+
+## PYTHON
+len(AnyVariableName)
+
+## JAVASCRIPT
+AnyVariableName.length;
+
+## GO (GOLANG)
+
+
+
+
+
+## IF / ELSE CONDITIONALS
+## IF / ELSE CONDITIONALS
+## IF / ELSE CONDITIONALS
+
+## PYTHON
+if WhateverCondition:
+    print("If WhateverCondition is True, then do whatever conditional operation")
+else:
+    print("...else:  then do something else")
+
+## JAVASCRIPT
+if (WhateverCondition) {
+     console.log("If WhateverCondition is true");
+} else {
+     console.log("else AlternateCondition goes here");
+}
+
+## GO (GOLANG)
+
+
+## IF / ELSE IF / ELSE CONDITIONALS
+## IF / ELSE IF / ELSE CONDITIONALS
+## IF / ELSE IF / ELSE CONDITIONALS
+
+## PYTHON
+if WhateverCondition:
+    return()
+elif ElseIfCondition:
+    return()
+else:
+    return()
+
+## JAVASCRIPT
+if (WhateverCondition) {
+  return();
+} else if (ElseIfCondition) {
+  return();
+} else {
+  return();
+}
+
+## GO (GOLANG)
+
+
+
+
+## EQUIVALENCY OPERATOR
+## EQUIVALENCY OPERATOR
+## EQUIVALENCY OPERATOR
+
+## PYTHON
+if 1 == 1:  ## WhateverCondition
+    print("If WhateverCondition is True, then do whatever conditional operation")
+else:
+    print("...else:  then do something else")
+
+## JAVASCRIPT
+if (1 == 1) { // WhateverCondition
+     console.log("If WhateverCondition is true");
+} else {
+     console.log("else AlternateCondition goes here");
+}
+
+## GO (GOLANG)
+
+
+
+
+
+
+
+## CREATE LIST / ARRAY
+## CREATE LIST / ARRAY
+## CREATE LIST / ARRAY
+
+## PYTHON
+PythonList = []
+
+# JAVASCRIPT
+var JavascriptArray = [];
+let JavascriptArray = [];
+
+## GO (GOLANG)
+
+
+
+
+## ADD ELEMENT TO LIST / ARRAY
+## ADD ELEMENT TO LIST / ARRAY
+## ADD ELEMENT TO LIST / ARRAY
+
+## PYTHON
+PythonList.append(AnyVariableName)
+
+# JAVASCRIPT
+var JavascriptArray.push(AnyVariableName);
+
+## GO (GOLANG)
+
+
+
+## POP ELEMENT FROM LIST / ARRAY
+## POP ELEMENT FROM LIST / ARRAY
+## POP ELEMENT FROM LIST / ARRAY
+
+## PYTHON
+PythonList.pop(AnyVariableName)
+
+# JAVASCRIPT
+var JavascriptArray.pop(AnyVariableName);
+
+## GO (GOLANG)
+
+
+## CREATE PYTHON DICTIONARY / JAVASCRIPT OBJECT
+## CREATE PYTHON DICTIONARY / JAVASCRIPT OBJECT
+## CREATE PYTHON DICTIONARY / JAVASCRIPT OBJECT
+
+## PYTHON
+PythonDictionary = {}
+
+# JAVASCRIPT
+var JavascriptObject = {};
+
+## GO (GOLANG)
+
+
+
+
+## FOR LOOPS
+## FOR LOOPS
+## FOR LOOPS
+
+## PYTHON
+i = 0   ## INITIALIZE COUNTER VARIABLE
+PythonList = []  ## CREATE EMPTY PYTHON LIST
+RangeOfIntegerNumbers = list(range(5)) ## SET RANGE OF INTEGERS FOR FOR LOOP
+for each in RangeOfIntegerNumbers:
+    PythonList.append(each)
+    i += 1 ## i = i + 1  ## INCREMENT COUNTER
+
+## JAVASCRIPT
+var JavascriptArray = [];   ## CREATE EMPTY JAVASCRIPT ARRAY
+for (var i = 0; i < 5; i++) {  ## INITIALIZE COUNTER; SET RANGE OF INTEGERS FOR FOR LOOP; INCREMENT COUNTER
+  JavascriptArray.push(i);
+}
+
+## GO (GOLANG)
+
+
+
+## GAME OVER
+    
+## WE HOPE YOU ENJOYED AND THAT THIS HELPS YOUR UNDERSTANDING OF USING PYTHON / GO / JAVASCRIPT LANGUAGEs TO SOLVE PROBLEMS WITH PROGRAMMING
+## PLEASE COME BACK AGAIN SOON
+## PLEASE VISIT OUR WEB SITES (OUR PROBLEM-SOLVING PROGRAMMING, CODING, & DEVELOPMENT SERVICES ARE AVAILABLE FOR HIRE):
+## www.JerusalemProgrammer.com
+## www.JerusalemProgrammers.com
+## www.JerusalemProgramming.com
