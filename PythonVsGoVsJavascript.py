b9151bebfc3be7b4fa7a8e1f41adea6f071b5280
JerusalemProgramming.com <jerusalemprogramming@gmail.com>
2019-02-26 20:31:27 +0200

    Update PythonVsGoVsJavascript.py

--- PythonVsGoVsJavascript.py
+++ PythonVsGoVsJavascript.py
@@ -240,11 +240,16 @@ for (var i = 0; i < 5; i++) {  ## INITIALIZE COUNTER; SET RANGE OF INTEGERS FOR 
   JavascriptArray.push(i);
 }
 
-## GO (GOLANG)
+## GO (GOLANG) - VERSION 1
 i := 0
 for i <= 5 {
-	fmt.Println(i) // CHANGE THIS TO ARRAY LATER
-	i = i + 1
+     fmt.Println(i) // CHANGE THIS TO ARRAY LATER
+     i = i + 1
+}
+
+## GO (GOLANG) - VERSION 2
+for i := 1; i <= 5; i++ {
+     fmt.Println(i) // CHANGE THIS TO ARRAY LATER
 }
 
 
